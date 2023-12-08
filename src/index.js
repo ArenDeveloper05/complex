c65c98fdc94a628b3bee5b2448f446f187276241
KarenSukiasyan05 <sukiasyank7@gmail.com>
2023-12-08 16:44:12 +0400

    services responsive

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-import "./styles/media.css";
-
 import "./i18n";
-import "./index.scss";
-import { Provider } from "react-redux";
 import store from "./redux/store";
+
+import "./index.scss";
+import "./styles/media.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
