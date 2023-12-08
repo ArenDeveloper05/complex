7cae5fb7001994a13cfd881c3644a578f42f7f20
ArenDeveloper05 <antisystem005@mail.ru>
2023-12-08 12:44:33 +0400

    home-top-section initial

---
+++ src/components/home/home-top/HomeTop.jsx
@@ -0,0 +1,10 @@
+const HomeTop = () => {
+  return (
+    <div className="home-top">
+      <div className="home-top-img"></div>
+      <div className="home-top-footer"></div>
+    </div>
+  );
+};
+
+export default HomeTop;
