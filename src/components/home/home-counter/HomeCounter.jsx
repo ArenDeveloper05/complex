832ced6b7bfca1980f6602309491424399874a87
Meri Hovhannisyan <meri.h1298@gmail.com>
2023-12-11 15:42:52 +0400

    icons

--- src/components/home/home-counter/HomeCounter.jsx
+++ src/components/home/home-counter/HomeCounter.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import CountUp from "react-countup";
 
 import userIcon from "../../../assets/images/user.png";
+import atomIcon from "../../../assets/images/atom.png";
+import touchIcon from "../../../assets/images/touch.png";
+import settingsIcon from "../../../assets/images/settings.png";
 
 import "./HomeCounter.scss";
 import Lines from "../../common/lines/Lines";
@@ -14,7 +17,7 @@ const HomeCounter = () => {
       <div className="home-counter-container">
         <div className="home-counter-container-box">
           <div className="home-counter-container-box-content">
-            <img src={userIcon} alt="" />
+            <img src={userIcon} alt="" width={50} height={50} />
             <CountUp start={0} end={1035} duration={2} delay={0}>
               {({ countUpRef }) => <span ref={countUpRef}></span>}
             </CountUp>
@@ -25,7 +28,7 @@ const HomeCounter = () => {
         </div>
         <div className="home-counter-container-box">
           <div className="home-counter-container-box-content">
-            <img src={userIcon} alt="" />
+            <img src={atomIcon} alt="" width={50} height={50} />
             <CountUp start={0} end={958} duration={3} delay={0}>
               {({ countUpRef }) => <span ref={countUpRef} />}
             </CountUp>
@@ -36,7 +39,7 @@ const HomeCounter = () => {
         </div>
         <div className="home-counter-container-box">
           <div className="home-counter-container-box-content">
-            <img src={userIcon} alt="" />
+            <img src={touchIcon} alt="" width={50} height={50} />
             <CountUp start={0} end={435} duration={3} delay={0}>
               {({ countUpRef }) => <span ref={countUpRef} />}
             </CountUp>
@@ -47,7 +50,7 @@ const HomeCounter = () => {
         </div>
         <div className="home-counter-container-box">
           <div className="home-counter-container-box-content">
-            <img src={userIcon} alt="" />
+            <img src={settingsIcon} alt="" width={50} height={50} />
             <CountUp start={0} end={1236} duration={2} delay={0}>
               {({ countUpRef }) => <span ref={countUpRef} />}
             </CountUp>
