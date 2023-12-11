74178b40ee6b18a54c47d6ff4dc292e88355fba2
ArenDeveloper05 <antisystem005@mail.ru>
2023-12-11 15:32:51 +0400

    aos

--- src/components/home/home-counter/HomeCounter.jsx
+++ src/components/home/home-counter/HomeCounter.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CountUp from "react-countup";
+import Lines from "../../common/lines/Lines";
 
 import userIcon from "../../../assets/images/user.png";
 
 import "./HomeCounter.scss";
-import Lines from "../../common/lines/Lines";
+
 const HomeCounter = () => {
   return (
     <div className="home-counter">
-      <p className="home-counter-info">some facts</p>
-      <p className="home-counter-title">We always ready for a challenge</p>
+      <p className="home-counter-info" data-aos="fade-down">
+        some facts
+      </p>
+      <p className="home-counter-title" data-aos="fade-down">
+        We always ready for a challenge
+      </p>
       <Lines bcg={"green"} />
       <div className="home-counter-container">
         <div className="home-counter-container-box">
-          <div className="home-counter-container-box-content">
+          <div
+            className="home-counter-container-box-content"
+            data-aos="fade-right"
+          >
             <img src={userIcon} alt="" />
             <CountUp start={0} end={1035} duration={2} delay={0}>
               {({ countUpRef }) => <span ref={countUpRef}></span>}
@@ -23,7 +31,7 @@ const HomeCounter = () => {
             <p>clients satisfaction</p>
           </div>
         </div>
-        <div className="home-counter-container-box">
+        <div className="home-counter-container-box" data-aos="fade-up">
           <div className="home-counter-container-box-content">
             <img src={userIcon} alt="" />
             <CountUp start={0} end={958} duration={3} delay={0}>
@@ -34,7 +42,7 @@ const HomeCounter = () => {
             <p>great projects</p>
           </div>
         </div>
-        <div className="home-counter-container-box">
+        <div className="home-counter-container-box" data-aos="fade-up">
           <div className="home-counter-container-box-content">
             <img src={userIcon} alt="" />
             <CountUp start={0} end={435} duration={3} delay={0}>
@@ -46,7 +54,10 @@ const HomeCounter = () => {
           </div>
         </div>
         <div className="home-counter-container-box">
-          <div className="home-counter-container-box-content">
+          <div
+            className="home-counter-container-box-content"
+            data-aos="fade-left"
+          >
             <img src={userIcon} alt="" />
             <CountUp start={0} end={1236} duration={2} delay={0}>
               {({ countUpRef }) => <span ref={countUpRef} />}
