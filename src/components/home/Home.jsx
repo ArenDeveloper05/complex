2e18529429df4f6bc7fdd5cc8e2b7db43c189d4d
KarenSukiasyan05 <sukiasyank7@gmail.com>
2023-12-27 16:33:47 +0400

    parallax section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,6 +10,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 import "./Home.scss";
+import Parallax from "../common/parallax/Parallax";
 
 const Home = () => {
   useEffect(() => {
@@ -20,8 +21,22 @@ const Home = () => {
     <div className="home">
       <HomeSlider />
       <HomeCounter />
+      <Parallax
+        bcg={
+          "https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1703635200&semt=sph"
+        }
+        title={"Parallax"}
+        txt={"some txt"}
+      />
       <HomeTop />
       <HomeRegister />
+      <Parallax
+        bcg={
+          "https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1703635200&semt=sph"
+        }
+        title={"Parallax"}
+        txt={"some txt"}
+      />
       <HomePartners />
     </div>
   );
