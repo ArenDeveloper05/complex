eca9d4ae27ba7103310bc577001dd89fc4a32440
ArenDeveloper05 <antisystem005@mail.ru>
2024-01-15 14:29:57 +0400

    header fonts

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
+
 import Contact from "../components/contact/Contact";
 import Layout from "../layout/Layout";
 
 const ContactPage = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
   return (
     <Layout>
       <Contact />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
+
 import Home from "../components/home/Home";
 import Layout from "../layout/Layout";
 
 const HomePage = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
   return (
     <Layout>
       <Home />
