17566abb36e5796be3fb9c77fcb948b35f0ac590
ArenDeveloper05 <antisystem005@mail.ru>
2024-01-15 14:02:29 +0400

    changes

--- src/components/header/header-auth-panel/HeaderAuthPanel.jsx
+++ src/components/header/header-auth-panel/HeaderAuthPanel.jsx
@@ -1,11 +1,8 @@
-import HeaderAuthPanelContact from "./header-auth-panel-contact/HeaderAuthPanelContact";
 import HeaderAuthPanelLang from "./header-auth-panel-lang/HeaderAuthPanelLang";
 
 const HeaderAuthPanel = () => {
   return (
     <div className="header-inner-auth-panel">
-      {/* <HeaderAuthPanelContact /> */}
-
       <HeaderAuthPanelLang />
     </div>
   );
