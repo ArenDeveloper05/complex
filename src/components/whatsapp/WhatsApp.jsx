569f525104ef15cd019b6cb401456b853d0a7cbc
ArenDeveloper05 <antisystem005@mail.ru>
2024-01-18 13:04:54 +0400

    whatsapp plugin

--- src/components/whatsapp/WhatsApp.jsx
+++ src/components/whatsapp/WhatsApp.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
-import whatsappIcon from "../../assets/images/icons/whatsappIcon.png";
+// import { ReactComponentElement as whatsappIcon } from "../../assets/images/icons/whatsappIcon.png";
 
 import "./WhatsApp.scss";
+import { WhatsAppWidget } from "react-whatsapp-widget";
+import "react-whatsapp-widget/dist/index.css";
 
 const WhatsApp = () => {
   return (
-    <div className="whatsapp">
-      <Link to={"/"}>
-        <img src={whatsappIcon} alt="whatsapp" title="Whats app" />
-      </Link>
-    </div>
+    // <div className="whatsapp">
+    //   <Link to={"/"}>
+    //     <img src={whatsappIcon} alt="whatsapp" title="Whats app" />
+    //   </Link>
+    // </div>
+    <WhatsAppWidget phoneNumber="37495656867" />
   );
 };
 
